7909415ae940f2b1af0a3164c7bec650412f16ed
Mugilvannan P <mugilvannanpichai02@gmail.com>
2024-02-25 08:17:17 +0530

    firebase

--- job-portal/src/firebase/firebase.config.js
+++ job-portal/src/firebase/firebase.config.js
@@ -1,23 +1,40 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBNsphXbcZrnpHlrz-cPw5s3rPCQawnoWY",
+//   authDomain: "job-portal-399c2.firebaseapp.com",
+//   projectId: "job-portal-399c2",
+//   storageBucket: "job-portal-399c2.appspot.com",
+//   messagingSenderId: "664732335064",
+//   appId: "1:664732335064:web:c634d41798b1fa58b050ad",
+//   measurementId: "G-N32PDH6DS3"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+
+// export default app;
+
+
 import { initializeApp } from "firebase/app";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBNsphXbcZrnpHlrz-cPw5s3rPCQawnoWY",
-  authDomain: "job-portal-399c2.firebaseapp.com",
-  projectId: "job-portal-399c2",
-  storageBucket: "job-portal-399c2.appspot.com",
-  messagingSenderId: "664732335064",
-  appId: "1:664732335064:web:c634d41798b1fa58b050ad",
-  measurementId: "G-N32PDH6DS3"
+  apiKey: "AIzaSyA9Vw4xA624QzsPZodRaJgd_ReOi6K1X2U",
+  authDomain: "job-portal-a96b3.firebaseapp.com",
+  projectId: "job-portal-a96b3",
+  storageBucket: "job-portal-a96b3.appspot.com",
+  messagingSenderId: "827283715524",
+  appId: "1:827283715524:web:539f45829d2024edda6091"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 export default app;
